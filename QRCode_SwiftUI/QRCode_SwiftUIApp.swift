d974c010c43c37618341657fbac3590822e94dc4
SeongMin-K <eo1tks3@naver.com>
2021-11-29 14:00:19 +0900

    Initial Commit

---
+++ QRCode_SwiftUI/QRCode_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCode_SwiftUIApp.swift
+//  QRCode_SwiftUI
+//
+//  Created by SeongMinK on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct QRCode_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
